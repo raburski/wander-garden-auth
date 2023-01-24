1f3332549d481bd407e0738b3591c2473e574ab9
Marcin Raburski <rabursky@gmail.com>
2023-01-24 12:35:08 +0700

    chore: no code guard added

--- api/foursquare.js
+++ api/foursquare.js
@@ -4,6 +4,9 @@ const redirect_url = 'http://wander.garden/auth.php?type=foursquare';
 
 export default async function handler(req, res) {
     const { code } = req.query;
+    if (!code) {
+        return res.status(400).send('code not provided')
+    }
 
     const url = `https://foursquare.com/oauth2/access_token?client_id=${client_id}&client_secret=${client_secret}&grant_type=authorization_code&redirect_uri=${redirect_url}&code=${code}`;
     const response = await fetch(url)
